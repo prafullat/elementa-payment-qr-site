3d776c611c75be3a46aa0b8f237324feabbe14b8
Prafulla Tekawade <prafulla_t@users.sourceforge.net>
2023-08-26 22:20:08 +0530

    Added some more content in the email body

--- src/scripts/qr-gen.js
+++ src/scripts/qr-gen.js
@@ -82,7 +82,7 @@ function generateQRCode () {
     if (volunteer == 1) {
         reloadButton.innerHTML = `<br><input type="button" name="reload" value="Save the payment confirmation and reload form" onclick="saveAndReloadTheForm();"/>`
     } else {
-        reloadButton.innerHTML = `<a href="mailto:elementaph1chs@gmail.com,customercare.elementa@gmail.com?Subject=Festival%20Collection%20for%20${building}-${flat}&Body=Dear Team,%0D%0A%0D%0APlease%20record%20this%20payment%20in%20Adda,%20Details%20below:%0D%0AUPI:%20${merchant_upi}%0D%0AName:%20${name.value}%0D%0AAmount:%20${amount.value}%0D%0AFlat:%20${building}-${flat}">
+        reloadButton.innerHTML = `<a href="mailto:elementaph1chs@gmail.com,customercare.elementa@gmail.com?Subject=Festival%20Collection%20for%20${building}-${flat}&Body=Dear%20Team,%0D%0A%0D%0APlease%20record%20this%20payment%20in%20Adda,%20Details%20below:%0D%0AUPI:%20${merchant_upi}%0D%0AName:%20${name.value}%0D%0AAmount:%20${amount.value}%0D%0AFlat:%20${building}-${flat}%0D%0AI%20have%20attached%20the%20payment%20confirmation%20screenshot%20with%20payment%20reference%20number.%0D%0AI%20confirm%20that%20without%20payment%20confirmation%20screenshot,%20Society%20will%20not%20be%20able%20to%20record%20the%20payment.">
         <input type="button" name="email_" value="Please email payment confirmation after successful payment">
         </input><br>
     </a><br>`
